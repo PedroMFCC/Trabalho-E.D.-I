ff1776e25b44a798a5d30710136ea9ffe9770ab8
Pedro Miguel Fernandes Campos Cardoso <pedromiguelfcc555@gmail.com>
2025-07-11 18:02:19 -0300

    Add files via upload

--- hash.c
+++ hash.c
@@ -4,6 +4,24 @@
 
 #include "hash.h"
 
+
+
+int hash(const char *cpf){
+    int h = 0;
+
+    for(int i = 0; cpf[i] != '\0'; i++){
+        h = (h + cpf[i]) % TAM;
+    }
+
+    return h;
+}
+
+void iniciaHash(Hash tabela){
+    for(int i = 0; i < TAM; i++){
+        tabela[i] = NULL;
+    }
+}
+
 void insereCliente(){
     FILE *arq = fopen("clientes.txt", "a");
     if(arq == NULL){
@@ -11,7 +29,21 @@ void insereCliente(){
         exit(1);
     }
 
-    
+    CLIENTE cliente;
+
+    getchar();
+    printf("CPF do cliente:");
+    scanf("%14[^\n]", cliente.cpf);
+    getchar();
+    printf("Nome do cliente:");
+    scanf("%80[\n]", cliente.nome);
+    getchar();
+    printf("Telefone do cliente:");
+    scanf("%20[\n]", cliente.telefone);
+    getchar();
+    scanf("%100[\n]", cliente.endereco);
+    getchar();
+    printf("\n----------------------\n");
 
 
 }
--- hash.h
+++ hash.h
@@ -1,14 +1,26 @@
 #ifndef HASH_H
 #define HASH_H
 
-#define TAM_HASH 127 
+#define TAM 127 
 
 typedef struct {
-    char cpf[12];      
+    char cpf[15];      
     char nome[81];
-    char telefone[16];
+    char telefone[21];
     char endereco[101];
 } CLIENTE;
 
+typedef struct Node {
+    CLIENTE cliente;
+    struct Node *prox;
+} Node;
+
+typedef Node *Hash[TAM];
+
+
+
+void iniciaHash(Hash tabela);
+void insereCliente();
+CLIENTE *buscarCliente(Hash tabela, const char *cpf)
 
 #endif
